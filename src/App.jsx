b793f704c3284ea8f1ed9402eb806cecc302b406
DARITH02 (DARITH02) <chandarith002@gmail.com>
2026-03-16 11:35:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AiFillApple } from "react-icons/ai";
+import AppRouter from "./routes/AppRouter";
+
+const App = () => {
+  return <AppRouter />;
+};
+
+export default App;
